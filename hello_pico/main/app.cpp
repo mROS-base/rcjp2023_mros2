766a3b66e3ed76101b94370a4517892870bac91a
s-hosoai <shintaro.hosoai@gmail.com>
2023-09-19 14:12:36 +0900

    STEP2: copy blink_led code

---
+++ hello_pico/main/app.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "led.h"
+
+extern "C" void app_main(void)
+{
+  led_init();
+  
+  while (1)
+  {
+    gpio_set_level(LED0, 1);
+    gpio_set_level(LED1, 1);
+    gpio_set_level(LED2, 1);
+    gpio_set_level(LED3, 1);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    gpio_set_level(LED0, 0);
+    gpio_set_level(LED1, 0);
+    gpio_set_level(LED2, 0);
+    gpio_set_level(LED3, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
