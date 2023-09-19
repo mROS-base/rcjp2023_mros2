6ba60f6398d20cdfc5d02ea1e841987395f93048
s-hosoai <shintaro.hosoai@gmail.com>
2023-09-19 13:42:57 +0900

    STEP1.5: refactor

--- blink_led/main/app.cpp
+++ blink_led/main/app.cpp
@@ -2,22 +2,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-
-#define LED0 GPIO_NUM_1
-#define LED1 GPIO_NUM_2
-#define LED2 GPIO_NUM_42
-#define LED3 GPIO_NUM_41
+#include "led.h"
 
 extern "C" void app_main(void)
 {
-  gpio_config_t config;
-  config.pin_bit_mask = (1ULL << LED0) | (1ULL << LED1) | (1ULL << LED2) | (1ULL << LED3);
-  config.mode = GPIO_MODE_OUTPUT;
-  config.intr_type = GPIO_INTR_DISABLE;
-  config.pull_down_en = GPIO_PULLDOWN_DISABLE;
-  config.pull_up_en = GPIO_PULLUP_DISABLE;
-  gpio_config(&config);
-
+  led_init();
+  
   while (1)
   {
     gpio_set_level(LED0, 1);
---
+++ blink_led/main/led.cpp
@@ -0,0 +1,12 @@
+#include "led.h"
+#include "driver/gpio.h"
+
+void led_init(){
+  gpio_config_t config;
+  config.pin_bit_mask = (1ULL << LED0) | (1ULL << LED1) | (1ULL << LED2) | (1ULL << LED3);
+  config.mode = GPIO_MODE_OUTPUT;
+  config.intr_type = GPIO_INTR_DISABLE;
+  config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  config.pull_up_en = GPIO_PULLUP_DISABLE;
+  gpio_config(&config);
+}
