a4c030d1f0e7cd6c4ab19859e4a86aa951f65ab3
s-hosoai <shintaro.hosoai@gmail.com>
2023-09-21 18:42:52 +0900

    feature drive by cmd_vel

--- components/pico_driver/motor.h
+++ components/pico_driver/motor.h
@@ -11,6 +11,7 @@
 #define PI 3.1415926535897932384626433832795
 #define MIN_HZ 80
 #define TIRE_DIAMETER (48.00)
+#define TREAD_WIDTH (65.00)
 #define PULSE (TIRE_DIAMETER * PI / 400.0)
 #define MIN_SPEED (MIN_HZ * PULSE)
 #define DUTY 511 // 10bit / 2 - 1
@@ -19,6 +20,7 @@
 #define LOW 0
 
 void motor_init();
+void drive_by_cmd_vel(double linear_x, double angular_z);
 void constant_speed(int len, int tar_speed);
 void accelerate(int len, int tar_speed);
 void decelerate(int len, int tar_speed);
